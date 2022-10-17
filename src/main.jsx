b0a294a2f77ba6f627fdaebb564d6f7c23cd63ce
nikania <nianemis@gmail.com>
2022-10-17 18:33:10 +0200

    old

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "antd/dist/antd.css";
-import HashConnectAPIProvider from "./useHashconnect";
+import HashConnectAPIProvider from "./useHashconnect_old";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
