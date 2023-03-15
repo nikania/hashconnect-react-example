8efb9dac5d9de90fcf93436219422efa98e45468
nikania <nianemis@gmail.com>
2023-03-15 14:58:53 +0100

    comment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// need polyfill for build (buffer)
+// ussue with polyfill https://github.com/remorses/esbuild-plugins/issues/24
+// https://github.com/vitejs/vite/issues/7384
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
